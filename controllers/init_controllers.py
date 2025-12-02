8af01371595730ca8e7334db77dedbf679e382fd
kauantarchetti <kauantarchetti@gmail.com>
2025-12-02 19:08:45 -0300

    fix bugs controllers and routes

--- controllers/init_controllers.py
+++ controllers/init_controllers.py
@@ -1,5 +1,19 @@
+from bottle import Bottle
 from .tarefa_controller import tarefa_routes
+from .user_controller import user_routes
+from .home_controller import home_routes
+
+from .tarefa_controller import load_controller as load_tarefa
+from .user_controller import load_controller as load_user
+from .home_controller import load_controller as load_home
 
 def init_controllers(app: Bottle):
+    load_tarefa()
+    load_user()
+    load_home()
     app.merge(tarefa_routes)
+    app.merge(user_routes)
+    app.merge(home_routes)
     print("✅ Rotas de tarefas inicializadas!")
+    
+
